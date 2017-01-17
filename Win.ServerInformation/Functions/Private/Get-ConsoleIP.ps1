7ea72832a3be605d8aeadcaad971fc079892ee9b
FooBartn <barton.joshua@gmail.com>
2017-01-17 01:47:39 -0600

    Updated Get-ConsoleIP to advanced function

--- Win.ServerInformation/Functions/Private/Get-ConsoleIP.ps1
+++ Win.ServerInformation/Functions/Private/Get-ConsoleIP.ps1
@@ -1,27 +1,38 @@
-Function Get-ConsoleIP ($Name,$Manufacturer) {
-    Write-Verbose "$Name is manufactured by $Manufacturer"
+Function Get-ConsoleIP () {
+    [CmdletBinding()]
+    param (
+        # Name of Server
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ComputerName,
+
+        # Manufacturer
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Manufacturer
+    )
+
+    $CommonParams = @{
+        ComputerName = $ComputerName
+        ErrorAction = 'Stop'
+    }
+
+    Write-Verbose "$ComputerName is manufactured by $Manufacturer"
     Write-Verbose "Checking for ILO/DRAC/etc"
+
     Switch -wildcard ($Manufacturer) {
         "*Dell*"	{
             try {
-                $DellParams = @{
-                    Namespace = 'root\cimv2\dell'
-                    ComputerName = $Name
-                    ErrorAction = 'Stop'
-                }
-                (Get-WmiObject Dell_RemoteAccessServicePort @DellParams).AccessInfo
+                $DellParams = @{Namespace = 'root\cimv2\dell'}
+                (Get-WmiObject Dell_RemoteAccessServicePort @CommonParams @DellParams).AccessInfo
             } catch {
                 "Dell Managment Tools Not Installed"
             }
         }
         "*HP*"	{
             try {
-                $HpParams = @{
-                    Namespace = 'root\HPQ'
-                    ComputerName = $Name
-                    ErrorAction = 'Stop'
-                }
-                (Get-WmiObject -class HP_ManagementProcessor @HpParams).IPAddress
+                $HpParams = @{Namespace = 'root\HPQ'}
+                (Get-WmiObject -class HP_ManagementProcessor @CommonParams @HpParams).IPAddress
             } catch {
                 "HP Managment Tools Not Installed"
             }

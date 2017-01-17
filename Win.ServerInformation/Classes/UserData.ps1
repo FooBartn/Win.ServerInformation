a2589847c094ff9a9ed3b87916ae4dc659f886c4
FooBartn <barton.joshua@gmail.com>
2017-01-17 01:46:20 -0600

    Added UserData class

---
+++ Win.ServerInformation/Classes/UserData.ps1
@@ -0,0 +1,13 @@
+class UserData {
+    # Properties
+    [string] $ComputerName
+    [string] $IP
+    [string] $UserName
+    [string] $Description
+    [string] $SID
+    [int] $PasswordAge
+    [string] $LastLogin
+    [string] $UserFlags
+    [int] $BadPasswordAttempts
+    [int] $MaxBadPasswords
+}
